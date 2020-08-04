71af2d395303101b2cca471c75552e7c9dafb59f
Joseph Chingalo <profschinfalo@gmail.com>
2020-08-04 10:01:43 +0300

    feat: get analytic parameters values

--- src/app/pages/home/helpers/get-anlytics-parameters.ts
+++ src/app/pages/home/helpers/get-anlytics-parameters.ts
@@ -1,9 +1,31 @@
+import { DATA_CONFIG } from './get-chart-data-config';
+import * as _ from 'lodash';
+
 export function getAnlyticsParameters(
   selectedOrgUnitItems: any,
   selectedPeriods: any
 ) {
-  const pe = [];
-  const dx = [];
-  const ou = [];
+  const pe = _.uniq(
+    _.flattenDeep(_.map(selectedPeriods, (period: any) => period.id || []))
+  );
+  const dx = _.uniq(
+    _.flattenDeep(
+      _.map(DATA_CONFIG, (dataConfig: any) => {
+        return _.map(
+          dataConfig.series || [],
+          (seriesConfig: any) => seriesConfig.id || []
+        );
+      })
+    )
+  );
+  const ou = _.uniq(
+    _.flattenDeep(
+      _.map(
+        selectedOrgUnitItems,
+        (organistionUnit: any) => organistionUnit.id || []
+      )
+    )
+  );
+  console.log({ dx, pe, ou });
   return { dx, pe, ou };
 }
